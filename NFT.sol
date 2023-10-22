beb97936ab4cf705e93b7c8c5ddd889bf85b111e
Loïc Darboux <32105730+loicdrbx@users.noreply.github.com>
2023-10-22 09:35:42 -0400

    Add comments

--- NFT.sol
+++ NFT.sol
@@ -7,6 +7,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "https://github.com/erc6551/reference/blob/v0.2.0-deployment/src/interfaces/IERC6551Registry.sol";
 import "https://github.com/erc6551/reference/blob/v0.2.0-deployment/src/lib/ERC6551AccountLib.sol";
 
+/**
+ * @title NFT
+ * @dev A smart contract for creating and managing ERC721 Non-Fungible Tokens (NFTs) 
+ *      with the additional functionality required by token bound accounts.
+ */
 contract NFT is ERC721, Ownable {
 
     // State variables
@@ -16,6 +21,11 @@ contract NFT is ERC721, Ownable {
     IERC6551Registry public immutable registry;
     uint256 public immutable chainId;
 
+    /**
+     * @dev Constructor to initialize the contract.
+     * @param _implementation The address of the NFT implementation contract.
+     * @param _registry The address of the ERC6551 registry contract.
+     */
     constructor(
         address _implementation,
         address _registry       
@@ -24,6 +34,11 @@ contract NFT is ERC721, Ownable {
         registry = IERC6551Registry(_registry); 
     }
 
+    /**
+     * @dev Get the account associated with a given NFT token ID.
+     * @param tokenId The ID of the NFT token.
+     * @return The address of the associated account.
+     */
     function getAccount(uint256 tokenId) public view returns (address) {
         return
             registry.account(
@@ -35,6 +50,11 @@ contract NFT is ERC721, Ownable {
             );
     }
 
+    /**
+     * @dev Create an account associated with a given NFT token ID.
+     * @param tokenId The ID of the NFT token.
+     * @return The address of the newly created account.
+     */
     function createAccount(uint256 tokenId) public returns (address) {
         return
             registry.createAccount(
@@ -47,12 +67,20 @@ contract NFT is ERC721, Ownable {
             );
     }
 
+    /**
+     * @dev Add Ether to an account associated with a given NFT token ID.
+     * @param tokenId The ID of the NFT token.
+     */
     function addEth(uint256 tokenId) external payable {
         address account = getAccount(tokenId);
         (bool success, ) = account.call{value: msg.value}("");
         require(success, "Failed to send ETH");
     }
 
+    /**
+     * @dev Mint a new NFT and assign it to the owner.
+     * Only the owner of the contract can call this function.
+     */
     function safeMint() public onlyOwner {
         _safeMint(msg.sender, ++totalSupply);
     }
